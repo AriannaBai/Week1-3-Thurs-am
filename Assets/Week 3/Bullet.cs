f2b8db79321b24ff1c1f389240db939634f60315
Xiaofan Bai <baixia@shernet.sheridancollege.ca>
2025-01-27 00:23:32 -0500

    I fix some problem in the week 3 inclass

--- Assets/Week 3/Bullet.cs
+++ Assets/Week 3/Bullet.cs
@@ -6,15 +6,20 @@ public class Bullet : MonoBehaviour
 {
     public float speed = 5;
     bool hasBeenFired = false;
+
     void Update()
     {
         PointAtMouse();
+        if (hasBeenFired == true)
+        {
+
+        }
     }
 
     void PointAtMouse()
     {
-        Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mouse.z = 0;
+        Vector3 mouse = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        mouse.x = 0;
         Vector2 direction = mouse - transform.position;
 
         transform.up = direction;
@@ -22,6 +27,7 @@ public class Bullet : MonoBehaviour
 
     void Movement()
     {
-        
+        //transform.position += Vector3.up * speed * Time.deltaTime;
+        transform.Translate(transform.up * speed * speed * Time.deltaTime);
     }
 }
