0757f5de6d87e02b8a53a9205b16c3d4a69abef2
Xiaofan Bai <baixia@shernet.sheridancollege.ca>
2025-01-26 19:22:41 -0500

    I finished the sun curve and I draw the boat and  use the keyboard to control and also change the layer

---
+++ Assets/scripts/assignment 1/boat.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boat : MonoBehaviour
+{
+    public float Row = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = transform.position;
+
+        pos.x += Input.GetAxisRaw("Horizontal") * Row * Time.deltaTime;
+
+        transform.position = pos;
+    }
+}
--- Assets/scripts/assignment 1/sun.cs
+++ Assets/scripts/assignment 1/sun.cs
@@ -7,11 +7,11 @@ using UnityEngine;
 public class sun : MonoBehaviour
 {
     [Range(0, 1)]
-    public float time;
+    public float time; // this means the value of the curve like the number which I set is from zero to one
     public AnimationCurve curve;
 
-    public Transform start;
-    public Transform end;
+    public Transform start; // the sqaure shich is the start position of the object
+    public Transform end; //  the sqaure shich is the end position of the object
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +22,8 @@ public class sun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.Lerp(start.position, end.position, curve.Evaluate(time));
+
+        transform.position = Vector2.Lerp(start.position, end.position, curve.Evaluate(time)); // let the sun move by the curve and the start is where sun start moving and the end is the position where sun stop moving
+
     }
 }
