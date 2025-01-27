15766c1f0087fc024a0cca404b6a26a5e9405fa6
Xiaofan Bai <baixia@shernet.sheridancollege.ca>
2025-01-27 00:41:19 -0500

    fix some small problems

--- Assets/scripts/AnimationDemo.cs
+++ Assets/scripts/AnimationDemo.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class AnimationDemo : MonoBehaviour
 {
     public AnimationCurve curve;
-     // Start is called before the first frame update
-
-     void Start()
+    [Range(0, 1)]
+    public float t;
+    // Start is called before the first frame update
+    void Start()
     {
 
     }
@@ -15,6 +18,13 @@ public class AnimationDemo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        t += Time.deltaTime;
+
+        if (t>1)
+{
+            t = 0;
+        }
+        transform.localScale = Vector2.one * curve.Evaluate(t);
+
     }
 }
